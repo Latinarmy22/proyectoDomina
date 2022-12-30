daced6ae37e19d05e211741b83df74764eb54364
Latinarmy22 <radabeca23@gmail.com>
2022-12-30 06:56:42 -0500

    select llenado con el array

--- js/main.js
+++ js/main.js
@@ -1,12 +1,13 @@
+//se asegura de cargar la pagina para luego llamar a la funcion que llena el select.
 window.addEventListener('load', function() {
     addOptionsToSelect();   
 }); 
 
 function addOptionsToSelect(selectDelivery){
     const deliverys = ["Daniel Hernandez Agredo", "Sergio Perez" , "Carlos Menchaca", "Luna Uribe"];
-    let select = document.getElementById('selectDelivery')[0];
+    let select = document.getElementById('selectDelivery');
 
-    for(i=0;i<=deliverys.length;i++){
-        deliverys.options[i] = new deliverys
+    for(i=1;i<=deliverys.length;i++){
+        select.options[i] = new Option(deliverys[i-1]);
     }
 }
